43eada7923a8dd3fb0a3f4beae6d3009625e5750
Laica-Lunasys <laica.lunasys@gmail.com>
2020-11-19 14:51:43 +0900

    Split template definition

--- template/template.go
+++ template/template.go
@@ -18,19 +18,9 @@ type Template struct {
 
 	// Aircon template
 	Aircon *Aircon `json:"aircon,omitempty"`
-}
 
-type Aircon struct {
-	Operation *Action                `json:"operation"`
-	Modes     map[string]*AirconMode `json:"modes"`
-}
-
-type AirconMode struct {
-	Temp           *Action `json:"temp,omitempty"`
-	Humid          *Action `json:"humid,omitempty"`
-	Fan            *Action `json:"fan,omitempty"`
-	HorizontalVane *Action `json:"horizontal_vane,omitempty"`
-	VerticalVane   *Action `json:"vertical_vane,omitempty"`
+	// Light template
+	Light *Light `json:"light,omitempty"`
 }
 
 type Action struct {
