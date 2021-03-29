2f4b4c86616175e1ee4ab23c5276417944404084
Laica Lunasys <laica.lunasys@gmail.com>
2021-03-29 20:34:07 +0900

    Add RAW Format

--- hex/hex.go
+++ hex/hex.go
@@ -1,6 +1,24 @@
 package hex
 
 type HexCode struct {
+	Format   CodeFormat
 	Code     [][]int
+	Raw      []int
 	Interval int
 }
+
+type CodeFormat int32
+
+const (
+	// AEHA - Association for Electric Home Appliances
+	AEHA CodeFormat = iota
+
+	// WIP: NEC - NEC Format
+	// NEC
+
+	// WIP: SONY - Sony IR Format
+	// SONY
+
+	// RAW - Raw IR Format
+	RAW
+)
